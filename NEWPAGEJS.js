df9fd4df0af821574b86d140389421611ed0bb45
Wandalice <Wandalice@.github.com>
2020-12-29 14:53:14 +0100

    flowing

--- NEWPAGEJS.js
+++ NEWPAGEJS.js
@@ -3,14 +3,37 @@
 // sth spawn images below the pages
 const spawn = ( ) => {
   const src = "wanda/we1.jpg"
+  const src1 = "wanda/we2.jpg"
   var picture = document.createElement("img");
-  picture.src = src
+  var picture1 = document.createElement("img");
+    picture.src = src
+    picture1.src = src1
     picture.style.width = "30%"
-  document.querySelector(".artboard").appendChild(picture);
+    picture1.style.width = "20%"
+  document.querySelector(".artboard").appendChild(picture)
+  document.querySelector(".artboard").appendChild(picture1)
+
+  document.querySelector('.flow').style["animation-duration"] = "8s"
+
 
 }
 spawn()
 
+
+
+//picture.classList.add(".flow")
+//var animation = document.createElement(".flow");
+//picture.style.animationName = "flowing"
+//picture.sytle.animaionDuration = 4s
+//.classList.add(".flow");
+
+//document.querySelector(".artboard").appendChild("flow");
+//var animation = document.createElement(".flow");
+  //animation.style.flow.animationName = "flowing";
+
+
+  //picture.style.flow = document.getElementById(".flow");
+  //var animation = document.createElement(".flow")
 
 //given a path
 // all start at bottom- css animation randomly x,y
