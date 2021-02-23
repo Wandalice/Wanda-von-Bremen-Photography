59d62b670959632c8cddc5b91706cc5d0fa85da5
Wandalice <Wandalice@.github.com>
2021-02-23 12:09:43 +0100

    box version

--- NEWPAGEJSsimple.js
+++ NEWPAGEJSsimple.js
@@ -1,9 +1,20 @@
+
+//randdomly sign a background image to box1,box2 - first thing on the pages
+// box start from below - move up (translate stuff,) negative number
+
+// create new boxes in every draw cicle - if 10 s pass, create new box, add to array, stored in
+// add to array
+
 let secondsPassed;
 let oldTimeStamp;
 let fps;
 let boxposition = {x: 0, y: 0}
-let scale = [2, 0.1, 0.5, 3, 0.2]
+let box2position = {x: 0, y: 0}
+let scale = [2, 0.1, 0.5, 7, 0.2]
 let scale2 = {value: 0.3}
+let box2scale2 = {value: 0.9}
+let color = ['red', 'green', 'pink', 'black', 'blue', 'orange']
+
 
 
 
@@ -48,25 +59,50 @@ let scale2 = {value: 0.3}
     function draw(deltatime){
 
 
+
+
       const box = document.querySelector(".boxx")
-      const scaleoffset = (Math.random() * 0.2) -0.1
+      const box2 = document.querySelector(".boxx2")
+      //const scaleoffset = (Math.random() * 0.2) -0.1
       const numberscale = Math.floor(scale.length * Math.random())
       const newScale = scale[numberscale]
+      const numberscale2 = Math.floor(scale.length * Math.random())
+      const newScale2 = scale[numberscale2]
 
-      boxposition.x = boxposition.x+0.1
-      boxposition.y = boxposition.y+0.1
+      //const colorarea = Math.floor(color.length * Math.random())
+      //const newcolor = color[colorarea]
+      const randomr = Math.floor(Math.random() * 255)
+      const randomg = Math.floor(Math.random() * 255)
+      const randomb = Math.floor(Math.random() * 255)
+      const randomO = Math.random()
+
+      const random2r = Math.floor(Math.random() * 255)
+      const random2g = Math.floor(Math.random() * 255)
+      const random2b = Math.floor(Math.random() * 255)
+      const random2O = Math.random()
+
+      boxposition.x = boxposition.x+(Math.random() * 0.2)
+      boxposition.y = boxposition.y+(Math.random() * 0.1)
+
+      box2position.x = box2position.x+(Math.random() * 1)
+      box2position.y = box2position.y+(Math.random() * 2)
 
 
-      scale2.value = scale2.value+scaleoffset
+      //scale2.value = scale2.value+scaleoffset
 
 
 
 
 
-      let randomColor = Math.random() > 0.5? '#ff8081' : '#0099b0'
-      box.style.backgroundColor = "randomColor"
+      //let randomColor = Math.random() > 0.5? '#ff8081' : '#0099b0'
+      //box.style.backgroundColor = "randomColor"
       //box.fill(100, 50, 200, 175)
 
       box.style.transform = `translateX(${boxposition.x}px) translateY(${boxposition.y}px) scale(${newScale})`
+      box.style.backgroundColor = `rgba(${randomr},${randomg},${randomb},${randomO})`
+
+      box2.style.transform = `translateX(${box2position.x}px) translateY(${box2position.y}px) scale(${newScale2})`
+      box2.style.backgroundColor = `rgba(${random2r},${random2g},${random2b},${random2O})`
+      console.log(`translateX(${box2position.x}px) translateY(${box2position.y}px) scale(${newScale2})`)
 
     }
